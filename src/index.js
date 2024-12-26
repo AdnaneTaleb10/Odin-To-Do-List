de434a68761ea3697d079fbd8e4d541038f51df7
Adnane <adnane102464@gmail.com>
2024-12-26 17:32:28 +0100

    Add favicon via importing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import favicon from './Assets/to-do-list.png';
+function setFavicons(favicon){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel', 'shortcut icon');
+    setFavicon.setAttribute('href', favicon);
+    headTitle.appendChild(setFavicon);
+};
+setFavicons(favicon);
