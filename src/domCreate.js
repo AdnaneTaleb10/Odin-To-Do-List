58216e12fe8ae0aec54d1cb9e4e07523a3470feb
Adnane <adnane102464@gmail.com>
2024-12-26 17:33:30 +0100

    Add module to create DOM elements

---
+++ src/domCreate.js
@@ -0,0 +1,32 @@
+const create = (function(){
+    function createTextElement(element , text){
+        const item = document.createElement(`${element}`);
+        item.textContent = text;
+        return item;
+    };
+
+    function createElementWithID(element , id , text){
+        const item = document.createElement(`${element}`);
+        item.id = id;
+        item.textContent = text;
+        return item;
+    }
+
+    function createElementWithClass(element , className , text){
+        const item = document.createElement(`${element}`);
+        item.classList.add(`${className}`);
+        item.textContent = text;
+        return item;
+    }
+
+    function createImgWithSrc(src){
+        const img = document.createElement('img');
+        img.src = src;
+        return img;
+    }
+
+    return{createTextElement , createElementWithID , createElementWithClass , createImgWithSrc}
+
+})();
+
+export default create;
