f8b8ead99408739f2368d65ed03806699d52ef07
Adnane <adnane102464@gmail.com>
2024-12-26 18:53:46 +0100

    remove old implementation to add the favicon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import './style.css';
-import favicon from './Assets/to-do-list.png';
-function setFavicons(favicon){
-    let headTitle = document.querySelector('head');
-    let setFavicon = document.createElement('link');
-    setFavicon.setAttribute('rel', 'shortcut icon');
-    setFavicon.setAttribute('href', favicon);
-    headTitle.appendChild(setFavicon);
-};
-setFavicons(favicon);
+import setLogo from './logo';
+
