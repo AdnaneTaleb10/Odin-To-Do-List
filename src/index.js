15d60b655965d8ff94d2e69ac120b0235bf14f36
Adnane <adnane102464@gmail.com>
2025-01-23 21:23:25 +0100

    Make projects card structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import './css/style.css';
 import setLogo from './modules/logo';
-import Project from './modules/projectControls';
 import {Task , newTaskCard} from './modules/taskControls';
+import { Project , newProject } from './modules/projectControls';
 
 
 let tasksArr = [];
 let projectsArr = [];
 
-pushTask(new Task("Header", 'Restaurant Page', '24-06-2024', 'High', 'Header components: Logo, dark/light mode theme icon, and login button'));
+/* pushTask(new Task("Header", 'Restaurant Page', '24-06-2024', 'High', 'Header components: Logo, dark/light mode theme icon, and login button'));
 pushTask(new Task('Nav', 'Dashboard', '13-10-2024', 'low', 'nav icons'));
-pushTask(new Task('Main', 'Library', '10-10-2050', 'medium', 'libros'));
+pushTask(new Task('Main', 'Library', '10-10-2050', 'medium', 'libros')); */
 console.table(tasksArr);
+
+pushProject(new Project("Portfolio" , "myportfolio.com" , "website to showcase my personal projects"));
+console.table(projectsArr)
 
 function pushTask(task) {
     tasksArr.push(task)
@@ -18,6 +21,13 @@ function pushTask(task) {
 function pushProject(project) {
     projectsArr.push(project)
 };
+
+function displayProjects(){
+    for(const project of projectsArr){
+        newProject(project)
+        console.log("heeeeelloowwww")
+    }
+}
 
 function displayCards() {
     for(const task of tasksArr) {
@@ -37,6 +47,6 @@ function setDatasetIndex() {
 };
 
 setLogo();
-displayCards();
+displayProjects();
 
-export { tasksArr, projectsArr, displayCards};
+export { tasksArr, projectsArr, displayCards , displayProjects };
--- src/modules/projectControls.js
+++ src/modules/projectControls.js
@@ -1,5 +1,8 @@
+import create from "./domCreate";
+import { projectsArr , displayProjects } from "../index";
+
 class Project {
-    constructor(title, link , description , ) {
+    constructor(title, link , description) {
         this.title = title;
         this.link = link;
         this.description = description;
@@ -7,4 +10,51 @@ class Project {
     }
 }
 
-export default Project;
+function newProject(project){
+    const minView = document.querySelector("#min-view");
+
+    const projectCard = create.createElement("div");
+    projectCard.classList.add('project-card');
+
+    const stripe = create.createElement("div");
+    stripe.classList.add("project-stripe");
+    projectCard.appendChild(stripe);
+
+    const wrapper = create.createElement("div");
+    wrapper.classList.add("wrapper");
+    stripe.appendChild(wrapper);
+
+    const info = create.createElement("div");
+    info.classList.add("project-info");
+
+    const title = create.createElement("p");
+    title.classList.add("project-title");
+    title.textContent = `${project.title}`;
+
+    const link = create.createElement("a");
+    link.href = `${project.link}`;
+    link.textContent = `${project.link}`;
+
+    info.appendChild(title);
+    info.appendChild(link);
+
+    wrapper.appendChild(info);
+
+    const actions = create.createElement("div");
+    actions.classList.add("project-actions");
+
+    const editBtn = create.createElement('button');
+    const removeBtn = create.createElement('button');
+    editBtn.classList.add('fa-regular', 'fa-pen-to-square', 'edit-project');
+    removeBtn.classList.add('fa-regular', 'fa-square-minus', 'remove-project');
+
+    actions.appendChild(editBtn);
+    actions.appendChild(removeBtn);
+
+    wrapper.appendChild(actions);
+    projectCard.appendChild(wrapper);
+
+    minView.appendChild(projectCard)
+}
+
+export {Project , newProject};
