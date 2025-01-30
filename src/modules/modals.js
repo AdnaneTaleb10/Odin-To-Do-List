627af6571ae8eb067f261863ef508adc71b58c50
Adnane <adnane102464@gmail.com>
2025-01-30 08:10:07 +0100

    Start to implement submit button

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -5,8 +5,18 @@ const createTaskBtn = document.querySelector('#create-task');
 const createProjectBtn = document.querySelector('#create-project');
 const taskForm = document.querySelector('#task-form');
 const projectForm = document.querySelector('#project-form');
+const addTask = document.querySelector('#add-task');
+const addProject = document.querySelector('#add-project');
 
 function modals(){
+    addTask.addEventListener('click' , (event) => {
+        event.preventDefault();
+    })
+
+    addProject.addEventListener('click' , (event) => {
+        event.preventDefault();
+    })
+
     createBtn.addEventListener('click' , () => {
         optionsModals.style.visibility = 'visible';
         closeModalBehavior(optionsModals);
@@ -39,5 +49,14 @@ function closeModalBehavior(modal){
         }
     }
 }
+
+// function submitTask() {
+//     const title = document.querySelector('#task-title').value;
+//     const project = document.querySelector('#projects-dropdown').value;
+//     const priority = document.querySelector('#task-priority').value;
+//     const dueDate = document.querySelector('#due-date').value;
+//     const description = document.querySelector('#project-desrcription').value;
+//     let task = new Task(title, project)
+// };
 
 export default modals;
