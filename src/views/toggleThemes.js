af95c5c66b168740f90dea3aa40c80fe91ae43f5
AdnaneTaleb10 <adnane102464@gmail.com>
2025-02-21 19:30:11 +0100

    Toggle icons for dark/light theme

--- src/views/toggleThemes.js
+++ src/views/toggleThemes.js
@@ -1,43 +1,48 @@
-const toggleBtn = document.querySelector('#toggle-theme');
+const toggleBtn = document.querySelector("#toggle-theme");
+const themeIcon = document.querySelector("#theme-icon");
 
-toggleBtn.addEventListener('click', () => {
-    if(localStorage.getItem('theme') === 'dark'){
-        applyTheme.light();
-    } else{
-        applyTheme.dark();
-    };
+toggleBtn.addEventListener("click", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    applyTheme.light();
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+  } else {
+    applyTheme.dark();
+    themeIcon.classList.remove("fa-moon");
+    themeIcon.classList.add("fa-sun");
+  }
 });
 
-const applyTheme = (function(){
-    let root = document.querySelector(':root');
-    
-    function dark(){
-        root.style.setProperty('--background', '#343a40');
-        root.style.setProperty('--black', '#ced4da');
-        root.style.setProperty('--dark-gray', '#adb5bd');
-        root.style.setProperty('--default-gray', '#6c757d');
-        root.style.setProperty('--light-gray', '#495057');
-        root.style.setProperty('--borders-gray', '#6c757d');
-        localStorage.setItem('theme', 'dark');
-    };
-    
-    function light(){
-        root.style.setProperty('--background', '#FFFFFF');
-        root.style.setProperty('--black', '#495057');
-        root.style.setProperty('--dark-gray', '#6d6a75ff');
-        root.style.setProperty('--default-gray', '#adb5bdff');
-        root.style.setProperty('--light-gray', '#ebebeb');
-        root.style.setProperty('--borders-gray', '#D3D3D3');
-        localStorage.setItem('theme', 'light');
-    };
+const applyTheme = (function () {
+  let root = document.querySelector(":root");
 
-    return { dark, light }
+  function dark() {
+    root.style.setProperty("--background", "#343a40");
+    root.style.setProperty("--black", "#ced4da");
+    root.style.setProperty("--dark-gray", "#adb5bd");
+    root.style.setProperty("--default-gray", "#6c757d");
+    root.style.setProperty("--light-gray", "#495057");
+    root.style.setProperty("--borders-gray", "#6c757d");
+    localStorage.setItem("theme", "dark");
+  }
+
+  function light() {
+    root.style.setProperty("--background", "#FFFFFF");
+    root.style.setProperty("--black", "#495057");
+    root.style.setProperty("--dark-gray", "#6d6a75ff");
+    root.style.setProperty("--default-gray", "#adb5bdff");
+    root.style.setProperty("--light-gray", "#ebebeb");
+    root.style.setProperty("--borders-gray", "#D3D3D3");
+    localStorage.setItem("theme", "light");
+  }
+
+  return { dark, light };
 })();
 
-if(localStorage.getItem('theme') === 'dark'){
-    applyTheme.dark();
-} else{
-    applyTheme.light();
-};
+if (localStorage.getItem("theme") === "dark") {
+  applyTheme.dark();
+} else {
+  applyTheme.light();
+}
 
-export default applyTheme
+export default applyTheme;
